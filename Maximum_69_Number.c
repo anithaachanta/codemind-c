ca8623fa1717b59f926cc82393cc57bc24a6e49c
anithaachanta <119720845+anithaachanta@users.noreply.github.com>
2023-05-18 20:38:42 +0530

    #include<stdio.h> int main() {     int n,arr[100],b,i=0,j,k;     scanf("%d",&n);     for(k=n;k>0;k=k/10)     {         b=k%10;         arr[i]=b;         i++;     }     for(j=i-1;j>=0;j--)     {         if(arr[j]==6)         {             arr[j]=9;             break;         }     }     for(j=i-1;j>=0;j--)     {         printf("%d",arr[j]);     } }

--- Maximum_69_Number.c
+++ Maximum_69_Number.c
@@ -1,24 +1,24 @@
 #include<stdio.h>
 int main()
 {
-    int n,a[100],b,i=0,j,k;
+    int n,arr[100],b,i=0,j,k;
     scanf("%d",&n);
     for(k=n;k>0;k=k/10)
     {
         b=k%10;
-        a[i]=b;
+        arr[i]=b;
         i++;
     }
     for(j=i-1;j>=0;j--)
     {
-        if(a[j]==6)
+        if(arr[j]==6)
         {
-            a[j]=9;
+            arr[j]=9;
             break;
         }
     }
     for(j=i-1;j>=0;j--)
     {
-        printf("%d",a[j]);
+        printf("%d",arr[j]);
     }
 }
